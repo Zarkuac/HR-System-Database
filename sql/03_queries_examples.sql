108672c9426d4c784ccec69d3a55b5bee5f1cb29
Tsotne Zarkua <102588746+Zarkuac@users.noreply.github.com>
2025-11-03 14:03:57 +0400

    Update 03_queries_examples.sql

--- sql/03_queries_examples.sql
+++ sql/03_queries_examples.sql
@@ -11,3 +11,20 @@ FROM EMPLOYEES E
          LEFT JOIN POSITIONS P ON P.POSITION_ID = E.POSITION_ID
          LEFT JOIN DEPARTMENTS D ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
          LEFT JOIN EMPLOYEE_STATUSES ES ON ES.ID = E.STATUS
+
+-- Employees salaries with "StartDate" filter
+
+SELECT EMP_SAL.EMPLOYEE_ID                       AS "Employee ID",
+       E.FIRST_NAME || ' ' || E.LAST_NAME        AS "Employee",
+       D.NAME                                    AS "Department",
+       EMP_SAL.AMOUNT || '$'                     AS "Salary",
+       EMP_SAL.START_DATE                        AS "START DATE",
+       CASE
+           WHEN E.STATUS = 1 THEN 'Active'
+           WHEN E.STATUS = 2 THEN 'On Leave'
+           WHEN E.STATUS = 3 THEN 'Terminated'
+           WHEN E.STATUS = 4 THEN 'Resigned' END AS "Employee Status"
+FROM SALARIES EMP_SAL
+         LEFT JOIN EMPLOYEES E ON E.EMPLOYEE_ID = EMP_SAL.EMPLOYEE_ID
+         LEFT JOIN DEPARTMENTS D ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+WHERE EMP_SAL.START_DATE BETWEEN :DATE_FROM AND :DATE_TO
